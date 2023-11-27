d99b61e2a2e9123825c9f0f39f5c866b5ab52a08
TOHU <tomozou.huang@pwc.com>
2023-11-27 15:04:58 +0800

    updat 20231127 15:04

--- EzTool.Workbench.Plugins.ReportEditor.NET6/Views/Document/DocumentView.xaml.cs
+++ EzTool.Workbench.Plugins.ReportEditor.NET6/Views/Document/DocumentView.xaml.cs
@@ -28,29 +28,6 @@ namespace EzTool.Workbench.Plugins.ReportEditor.NET6.Views.Document
         {
             InitializeComponent();
         }
-
-        private void InsertTextButton_Click(object sender, RoutedEventArgs e)
-        {
-            var objTextBlock = new TextBlock();
-            var objContext = new ParagraphContext();
-            var objBinding = new Binding()
-            {
-                Path = new PropertyPath(nameof(ParagraphContext.ShowText)),
-                Source = objContext
-            };
-
-            l_objParagraphs.Add(objContext);
-            BindingOperations.SetBinding(objTextBlock, TextBlock.TextProperty, objBinding);
-            RichTextBoxProxy.Initial(MainBox).Document.Blocks.Append(objTextBlock);
-        }
-
-        private void UpdateTextButton_Click(object sender, RoutedEventArgs e)
-        {
-            l_objParagraphs.ForEach((objP) =>
-            {
-                objP.ShowText = DateTime.Now.ToString("G");
-            });
-        }
     }
 
     public class ParagraphContext :
