b65b872f65085e1c9d72ca24ba212ac9c23a310f
EzTool <62757684+EzTool@users.noreply.github.com>
2023-12-04 17:45:13 +0800

    update 20231204 17:45

---
+++ EzTool.Workbench.Plugins.ReportEditor.NET6/HumbleObjects/View/TableCellProxyBundle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace EzTool.Workbench.Plugins.ReportEditor.NET6.HumbleObjects.View
+{
+    public class TableCellProxyBundle :
+        ITableCellProxyBundle
+    {
+        #region -- 變數宣告 ( Declarations ) --   
+
+        private TableCellCollection pi_objCells;
+
+        #endregion
+
+        #region -- 建構/解構 ( Constructors/Destructor ) --
+
+        public TableCellProxyBundle(TableCellCollection pi_objCells)
+        {
+            this.pi_objCells = pi_objCells;
+        }
+
+        #endregion
+
+        #region -- 靜態方法 (Shared Method ) --
+
+        public static ITableCellProxyBundle Initial(TableCellCollection pi_objCells)
+        {
+            return new TableCellProxyBundle(pi_objCells);
+        }
+
+        #endregion
+
+        #region -- 介面實做 ( Implements ) - [ITableCellProxyBundle] --
+
+        public List<TableCellProxy> Cells
+        {
+            get { return new List<TableCellProxy>(); }
+        }
+
+        #endregion
+    }
+
+    public interface ITableCellProxyBundle
+    {
+        List<TableCellProxy> Cells { get; }
+    }
+}
---
+++ EzTool.Workbench.Plugins.ReportEditor.NET6/HumbleObjects/View/TableRowProxy.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Documents;
+
+namespace EzTool.Workbench.Plugins.ReportEditor.NET6.HumbleObjects.View
+{
+    public  class TableRowProxy:
+        ITableRowProxy
+    {
+        #region -- 變數宣告 ( Declarations ) --   
+
+        private TableRow l_objTableRow;
+
+        #endregion
+
+        #region -- 建構/解構 ( Constructors/Destructor ) --
+
+        public TableRowProxy(TableRow pi_objTableRow)
+        {
+            this.l_objTableRow = pi_objTableRow;
+        }
+
+        #endregion
+
+        #region -- 靜態方法 (Shared Method ) --
+
+        public static ITableRowProxy Initial(TableRow pi_objTableRow)
+        {
+            return new TableRowProxy(pi_objTableRow);
+        }
+
+        #endregion
+
+        #region -- 介面實做 ( Implements ) - [ITableRowProxy] --
+
+        public ITableCellProxyBundle Cells { get { return TableCellProxyBundle.Initial(l_objTableRow.Cells); } }
+
+        #endregion
+
+    }
+
+    public interface ITableRowProxy
+    {
+        ITableCellProxyBundle Cells { get; }
+    }
+}
